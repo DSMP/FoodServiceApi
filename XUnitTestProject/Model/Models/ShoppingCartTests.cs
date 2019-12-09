d20b5982f3a0284ef26dea5dd00623c57689f5e1
Damian Smug <smugdamian@gmail.com>
2019-12-09 16:00:37 +0000

    add: test for shopping cart

---
+++ XUnitTestProject/Model/Models/ShoppingCartTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTestProject.Model.Models
+{
+    public class ShoppingCartTests
+    {
+        public void AddToBasketTest()
+        {
+            throw new NotImplementedException();
+        }
+        public void RemoveFromBasket()
+        {
+            throw new NotImplementedException();
+        }
+        public void CheckTotalPriceAfterAdded()
+        {
+            throw new NotImplementedException();
+        }
+        public void CheckTotalPriceAfterRemove()
+        {
+            throw new NotImplementedException();
+        }
+        public void CheckDiscountedPriceAfterAdded()
+        {
+            throw new NotImplementedException();
+        }
+        public void CheckDiscountedPriceAfterRemove()
+        {
+            throw new NotImplementedException();
+        }
+        public void CheckAppliedValidVoucher()
+        {
+            throw new NotImplementedException();
+        }
+        public void CheckAppliedInvalidVoucher()
+        {
+            throw new NotImplementedException();
+        }
+        public void CheckDiscountedPriceAfterAppliedValidVoucher()
+        {
+            throw new NotImplementedException();
+        }
+        public void CheckDiscountedPriceAfterAppliedInValidVoucher()
+        {
+            throw new NotImplementedException();
+        }
+        public void GetInfoAfterAppliedValidVoucher()
+        {
+            throw new NotImplementedException();
+        }
+        public void GetInfoAfterAppliedInValidVoucher()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
