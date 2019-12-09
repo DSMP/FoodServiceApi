3ed4cf7ef4b532244c17ac19b8185c42ebc67c57
Damian Smug <smugdamian@gmail.com>
2019-12-09 14:40:28 +0000

    add: out of stock tests

--- XUnitTestProject/Model/Services/ProductServiceTest.cs
+++ XUnitTestProject/Model/Services/ProductServiceTest.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -29,6 +30,28 @@ namespace XUnitTestProject.Model.Services
                 context.SaveChanges();
                 IProductService productService = new ProductService(context);
                 Assert.NotEmpty(productService.GetAll());
+            }
+        }
+        [Fact]
+        public void CheckIsProductOutOfStock()
+        {
+            using (var context = new FoodServiceContext(_optionsBuilder.Options))
+            {
+                context.Products.Add(new Product { Name = "sdsd", Quantity = 2 });
+                context.SaveChanges();
+                IProductService productService = new ProductService(context);
+                Assert.True(productService.IsProductOutOfStock(context.Products.First().ProductId));
+            }
+        }
+        [Fact]
+        public void CheckIsNotProductOutOfStock()
+        {
+            using (var context = new FoodServiceContext(_optionsBuilder.Options))
+            {
+                context.Products.Add(new Product { Name = "sdsd", Quantity = 0 });
+                context.SaveChanges();
+                IProductService productService = new ProductService(context);
+                Assert.False(productService.IsProductOutOfStock(context.Products.First().ProductId));
             }
         }
     }
